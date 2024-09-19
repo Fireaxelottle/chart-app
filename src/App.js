4afb96b70b3ddeeb68022cc538e8ca944dc5e73d
Fireaxelottle <asgharnaseem1975@gmail.com>
2024-09-19 20:48:26 +0500

    Final App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React , { useState } from 'react';
 import './App.css';
+import PopChart from './components/PopChart';
+import { Chart } from 'chart.js';
 
 function App() {
+  const [data, setData] = useState({
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Population',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        backgroundColor: ["rgb(75, 192, 192)" , "rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)", "rgb(153, 102, 255)", "rgb(201, 203, 207)", "rgb(255, 99, 132)"],
+        
+      }
+    ]
+  });
+  const [options, setOptions] = useState({});
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PopChart-2</h1>
+      <PopChart data={data} options={options}/>
     </div>
+      
+    
   );
 }
 
